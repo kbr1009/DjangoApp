dd777c3677fe9f1c235bd695ad821ad771dad043
kbr1009 <reo109r@icloud.com>
2020-09-09 21:08:03 +0900

    use listview

--- front/views.py
+++ front/views.py
@@ -3,9 +3,15 @@ from django.views import generic
 from .models import Product
 # Create your views here.
 
-class IndexView(generic.TemplateView):
-    template_name = 'front/index.html'
+#class IndexView(generic.TemplateView):
+#    template_name = 'front/index.html'
 
-    def get(self, request, *args, **kwargs):
-        products = Product.objects.all()
-        return render(request, self.template_name,{'products':products})
+#    def get(self, request, *args, **kwargs):
+#        products = Product.objects.all()
+#        return render(request, self.template_name,{'products':products})
+
+# listviewを使用した時
+
+class IndexView(generic.Listview):
+    template_name = 'front/index.html'
+    modele = Product
