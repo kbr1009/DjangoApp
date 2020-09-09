a837144efae57e9f9b0e265f14351442337c0fea
kbr1009 <reo109r@icloud.com>
2020-09-10 02:38:17 +0900

    検索昨日の実装

--- front/views.py
+++ front/views.py
@@ -14,6 +14,13 @@ class IndexView(generic.ListView): #Listviewを使用した時
     template_name = 'front/index.html'
     model = Product
 
+    def get_queryset(self):# （検索機能）　https://www.youtube.com/watch?v=37y8P1jmvMU&list=PL8EQJPo_jL6uwdvohYo4CWWheonZPNuE6&index=5
+        queryset = Product.objects.all()#上のyoutubeの4：00台を参照
+        if 'query'in self.request.GET:
+            qs = self.request.GET['query']
+            queryset = queryset.filter(product_name=qs)
+        return queryset
+
 
 class ProductsDetail(generic.DetailView):
     template_name = 'front/detail.html'
