9c0facf4bd807d8204a5fcf33c5be57bc81ac7c8
kbr1009 <reo109r@icloud.com>
2020-09-10 04:25:28 +0900

    インデックスの修正

--- front/urls.py
+++ front/urls.py
@@ -5,6 +5,6 @@ app_name = 'front'
 
 urlpatterns = [
     path('', views.IndexView.as_view(), name = 'index'),
-    path('/detail/<int:pk>',views.ProductsDetail.as_view(),name='detail'), #<int:pk>のpkはプライマリキーの略
+    path('detail/<int:pk>',views.ProductsDetail.as_view(),name='detail'), #<int:pk>のpkはプライマリキーの略
     #path('/products/',views.IndexView.as_view(),name='product-list')#youtube　しょう先生の【Python Webアプリ 中級】#5 の検索機能参照
 ]
